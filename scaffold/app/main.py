725b3a25ca635f162ff866845c3746341d2fc9e3
Lucasatgithub <bigbenle2016@gmail.com>
2026-05-02 08:46:01 +0800

    added CORS

--- scaffold/app/main.py
+++ scaffold/app/main.py
@@ -3,7 +3,16 @@ from fastapi import FastAPI
 from .database import Base, engine
 from .routes import router
 
+from fastapi.middleware.cors import CORSMiddleware
+
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="QR Code Generator Prototype")
 app.include_router(router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], # For development only
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
